bd0c060c6a00f686cc435af730691238c67e1bb4
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-12-03 23:05:15 -0500

    Update plot_methods.py

--- src/plot_methods.py
+++ src/plot_methods.py
@@ -51,17 +51,8 @@ def generate_graph_directed(): ...
 
 
 
-def load_matrix(file):
-    data = loadmat(file)
-    sorted(data.keys())
 
-    n = 360
-    val_matrix = np.zeros((n, n, 3))
-    link_matrix = np.zeros((n, n, 3))
 
-    val_matrix[:, :, 0] = data['vallag0']
-    val_matrix[:, :, 1] = data['vallag1']
-    val_matrix[:, :, 2] = data['vallag2']
 
 
     link_matrix[:, :, 0] = data['linkLag0']
@@ -72,50 +63,17 @@ def load_matrix(file):
 
 
 
-def make_graph_matrix(file: str):
-    lm, _= load_matrix(file)
-    G:Graph = nx.from_numpy_matrix(lm[:, :, 0])
-    remove_not_linked_nodes(G)
     return G
 
 
 
 
 
-def get_data_dict(data):
-    rowcount=0
-    newdata= dict()
-    for row in data:
-        rowcount+=1
-        columncount=0
-        for column in row:
-            columncount+=1
-            if column != 0:
-                if newdata.get(rowcount):
-                    newdata[rowcount].append(columncount)
-                else:
-                    newdata[rowcount] = [columncount]
-    return newdata
 
-def load_dict(file: str):
-    data = loadmat(file)
-    linklag0 = get_data_dict(data['linkLag0'])
 
     return linklag0
 
-def make_graph(file: str):
-    G:Graph= Graph()
-    linkl= load_dict(file)
 
-    for node in linkl:
-        G.add_node(node, label= str(node), title= str(node),
-                    scaling= {'min': 10, 'max': 30, 'label': {'enabled': True, 'min': 14, 'max': 30, 'maxVisible': 30, 'drawThreshold': 5}},
-                    shadow= {'enabled': True, 'color': 'rgba(0,0,0,0.5)', 'size': 10, 'x': 5, 'y': 5},
-                    shapeProperties= {'borderDashes': False, 'borderRadius': 6, 'interpolation': False, 'useImageSize': False, 'useBorderWithImage': False})
-        for xnode in linkl[node]:
-            G.add_edge(node, xnode, color= 'black')
-            
-    return G
 
 
 
