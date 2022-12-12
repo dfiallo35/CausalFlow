d998274e68022a2586dcd157fbd9e227b6b3af9c
lauolivia <91337957+lauolivia@users.noreply.github.com>
2022-12-12 02:56:57 -0500

    edges_color

--- src/edges_color.py
+++ src/edges_color.py
@@ -5,26 +5,21 @@ import networkx as nx
 
 
         
-# def color_edges2(edges:list, min, max):
-#     """Set color to edges """    
-#     if(min < 0):
-#         max=max-min
-#         edges=[x-min for x in edges]
-#         min=0        
-#     range=max-min 
-#     # print("range: ",range)
-#     # sd=np.std(edges) 
-#     # print("sd: ",sd)
-#     # med=np.median(edges)
-#     # print("median: ",med)
-#     colors = {}    
-#     for edge in edges:
-#         # color2 = int(abs(edge-med)/sd)
-#         color=int(abs(edge-min)/range *220)
-#         # print("color 1: ",color)
-#         # print("color 2: ",color2)
-#         colors[edge]=to_hexa_rgb(color)
-#     return colors
+def color_edges_standard_deviation(edges:list, min, max):
+    """Set color to edges """    
+    if(min < 0):
+        max=max-min
+        edges=[x-min for x in edges]
+        min=0        
+    range=max-min 
+    sd=np.std(edges) 
+    med=np.median(edges)
+    colors = [] 
+    for edge in edges:
+        color=int(abs(edge-med)/sd *200)+25
+        print(color)
+        colors.append(_to_hexa_rgb(color))
+    return colors
 
        
 def _to_hexa_rgb(n: int):
@@ -47,7 +42,8 @@ def color_edges(edges:list, min, max):
     range=max-min 
     colors = []
     for edge in pos_edges:
-        color=int(abs(edge-min)/range *220)
+        color=int(abs(edge-min)/range *200)+25
+        print(color)
         colors.append(_to_hexa_rgb(color))
     return colors
 
@@ -66,24 +62,21 @@ def to_hexa_rgb(n: int, min, max):
         min=0        
     range=max-min 
     colors = []
-    color=int(abs(n-min)/range *220)
+    color=int(abs(n-min)/range *200)+25
     colors.append(_to_hexa_rgb(color))
     return colors
 
 
 
-
-    n= str(hex(n))[2:]
-    return '#' + 'ff' + n + n
-
-
-
-a=[0.02341, 0.0452, 0.0433, -0.14, 0.238915, 0.0433, -0.18794, 0.2315, 0.08433, 0.13244,-0.2315]
+a=[0.02341, 0.0452, 0.0433, -0.14, 0.238915, 0.0433, -0.18794, 0.2315, 0.08433, 0.13244,-0.2315,1]
 colors=color_edges(a, min(a), max(a))
-
-c2=[to_hexa_rgb(n, min(a), max(a)) for n in a]
 print(colors)
+c2=[to_hexa_rgb(n, min(a), max(a)) for n in a]
+# print(colors)
 print(c2)
+print()
+c3=color_edges_standard_deviation(a,min(a),max(a))
+print(c3)
 # colors=[colors[x] for x in colors]
 # print(colors)
 
