d51f2dcbec02d0d2c450e2d1ef4589d5f5664b54
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-12-14 13:48:02 -0500

    old

---
+++ src/old.py
@@ -0,0 +1,159 @@
+def get_data_linkLag(datalink, dataval):
+    '''
+    Create a dictionary from two lists
+    :param datalink: List of links
+    :type datalink: list
+    :param dataval: List of values
+    :type dataval: list
+    :return: Dictionary
+    :rtype: dict
+    '''
+    rowcount=0
+    newdata= dict()
+
+    for row in datalink:
+        rowcount+=1
+        columncount=0
+
+        for column in row:
+            columncount+=1
+
+            if column != 0:
+                if newdata.get(rowcount):
+                    newdata[rowcount][columncount]= dataval[rowcount-1][columncount-1]
+                else:
+                    newdata[rowcount] = {columncount: dataval[rowcount-1][columncount-1]}
+
+    return newdata
+
+def load_mat2(file: str, linkLag: str, valLag: str):
+    """
+    Load a dictionary from a .mat file
+    :param file: Path to the .mat file
+    :type file: str
+    :param linkLag: Name of the dictionary in the .mat file
+    :type linkLag: str
+    :param valLag: Name of the dictionary in the .mat file
+    :type valLag: str
+    :return: Dictionary
+    :rtype: dict
+    """
+    data = loadmat(file)
+    datalink = []
+
+    for i in data[linkLag]:
+        temp=[]
+        for j in i:
+            temp.append(j)
+        datalink.append(temp)
+    
+    dataval = []
+    for i in data[valLag]:
+        temp=[]
+        for j in i:
+            temp.append(round(j, 4))
+        dataval.append(temp)
+
+    return get_data_linkLag(datalink, dataval)
+
+
+def merge_lags(lags: list, n: int):
+    newlag= dict()
+    for i in range(n):
+        for j in range(n):
+            count= 1
+            edge= []
+            avg= 0
+            for lag in lags:
+                if lag.get(i) and lag[i].get(j) and i != j:
+                    avg+= lag[i][j]
+                    edge.append(str(count))
+                count+=1
+            if edge != []:
+                if newlag.get(i) == None:
+                    newlag[i]= {j: {'text': ','.join(edge), 'avg': avg/len(edge)}}
+                else:
+                    newlag[i][j]= {'text': ','.join(edge), 'avg': avg/len(edge)}
+    return newlag
+
+def make_directed_graph(file):
+    G= nx.DiGraph()
+    vallag_list= []
+    elements= list(loadmat(file).keys())
+
+    n=1
+    while 'vallag'+str(n) in elements and 'linkLag'+str(n) in elements:
+        vallag_list.append(('linkLag'+str(n), 'vallag'+str(n)))
+        n+=1
+    
+    lag_dicts= []
+    for lag in vallag_list:
+        lag_dicts.append(load_mat2(file, lag[0], lag[1]))
+
+    lags_merged= merge_lags(lag_dicts, len(loadmat(file)[vallag_list[0][0]]))
+
+    wlist=[]
+    for node in lags_merged:
+        for xnode in lags_merged[node]:
+            wlist.append(lags_merged[node][xnode]['avg'])
+    max, min= find_max_min_w(wlist)
+
+    for node in lags_merged:
+        G.add_node(node,
+                label= str(node),
+                title= str(node),
+                opacity=0.7,
+                border_color='black',
+                border_size=2,
+                color= 'gray')
+        for xnode in lags_merged[node]:
+            G.add_node(xnode,
+                label= str(node),
+                title= str(node),
+                opacity=0.7,
+                border_color='black',
+                border_size=2,
+                color= 'gray')
+            
+            # G.add_edge(node, xnode,
+            #         color= to_hexa_rgb(lags_merged[node][xnode]['avg'], max, min),
+            #         label= lags_merged[node][xnode]['text'],
+            #         arrow_color= to_hexa_rgb(lags_merged[node][xnode]['avg'], max, min),
+            #         arrow_color_hover= to_hexa_rgb(lags_merged[node][xnode]['avg'], max, min))
+            G.add_edge(node, xnode,
+                    color= '#00ffff',
+                    label= lags_merged[node][xnode]['text'],
+                    arrow_color= '#00ffff',
+                    arrow_color_hover= '#00ffff')
+    return G
+
+
+def make_graph_lag0(file: str):
+    '''
+    Create a graph from a .mat file
+    :param file: Path to the .mat file
+    :type file: str
+    :return: Graph
+    :rtype: Graph
+    '''
+    G:Graph= Graph()
+    linkl= load_mat2(file, 'linkLag0', 'vallag0')
+
+    wlist=[]
+    for node in linkl:
+        for xnode in linkl[node]:
+            wlist.append(linkl[node][xnode])
+    max, min= find_max_min_w(wlist)
+
+    for node in linkl:
+        G.add_node(node,
+                label= str(node),
+                title= str(node),
+                opacity=0.7,
+                border_color='black',
+                border_size=2,
+                color= 'gray')
+        for xnode in linkl[node]:
+            # G.add_edge(node, xnode, color= to_hexa_rgb(linkl[node][xnode], max, min))
+            G.add_edge(node, xnode, color= '#1fffff')
+    return G
