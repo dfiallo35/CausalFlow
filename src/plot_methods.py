d3df7863a0e174ae5e0744ea04498adf66f65c17
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-12-21 22:50:51 -0500

    rename and order

--- src/plot_methods.py
+++ src/plot_methods.py
@@ -31,7 +31,7 @@ def load_json(file:str):
     data :dict() = json.load(open(file))
     return data
 
-def load_json_graph(file:str):
+def load_json_file(file:str):
     '''
     Load json file
     :param file: path to json file
@@ -48,8 +48,6 @@ def load_mat(file:str):
     '''
     return loadmat(file)
 
-
-#check: not tested
 def save_json(file:str, data:dict):
     '''
     Save json file
@@ -85,8 +83,8 @@ def get_math_data(data:dict, linkLag:str, valLag:str, new_names: dict):
         for row, data_row in enumerate(data[linkl]):
             for column, data_column in enumerate(data_row):
                 if data_column != 0:
-                    newdata[linkl][new_names[row + 1]]= {}
-                    newdata[vall].append({'source': new_names[row + 1], 'target': new_names[column + 1], 'weight': data[vall][row][column]})
+                    newdata[linkl][row + 1]= {}
+                    newdata[vall].append({'source': row + 1, 'target': column + 1, 'weight': data[vall][row][column]})
         count+=1
         linkl= linkLag + str(count)
         vall= valLag + str(count)
@@ -94,7 +92,7 @@ def get_math_data(data:dict, linkLag:str, valLag:str, new_names: dict):
     graphdict= {
         'graph': {
             'directed': False,
-            'metadata': {},
+            'metadata': {'node_color': 'gray', 'node_opacity': 0.7, 'node_border_size': 2, 'node_border_color': 'black'},
 
             'nodes': {},
             'edges': [],
@@ -103,7 +101,10 @@ def get_math_data(data:dict, linkLag:str, valLag:str, new_names: dict):
     
     link, val= linkval[0]
     for node in newdata[link]:
-        graphdict['graph']['nodes'][node]= {'metadata': {'label': str(node), 'title': str(node), 'opacity': 0.7, 'border_color': 'black', 'border_size': 2, 'color': 'gray'}}
+        if new_names:
+            graphdict['graph']['nodes'][node]= {'metadata': {'label': new_names[str(node)], 'title': new_names[str(node)]}}
+        else:
+            graphdict['graph']['nodes'][node]= {'metadata': {'label': str(node), 'title': str(node)}}
     
     edges= set()
     edge_colors1= colors([c['weight'] for c in newdata[val]])
@@ -118,7 +119,7 @@ def get_math_data(data:dict, linkLag:str, valLag:str, new_names: dict):
     digraphdict= {
         'graph': {
             'directed': True,
-            'metadata': {},
+            'metadata': {'node_color': 'gray', 'node_opacity': 0.7, 'node_border_size': 2, 'node_border_color': 'black'},
 
             'nodes': {},
             'edges': [],
@@ -127,13 +128,16 @@ def get_math_data(data:dict, linkLag:str, valLag:str, new_names: dict):
     edges= dict()
     for link, val in linkval[1:]:
         for node in newdata[link]:
-            digraphdict['graph']['nodes'][node]= {'metadata': {'label': str(node), 'title': str(node), 'opacity': 0.7, 'border_color': 'black', 'border_size': 2, 'color': 'gray'}}
+            if new_names:
+                digraphdict['graph']['nodes'][node]= {'metadata': {'label': new_names[str(node)], 'title': new_names[str(node)]}}
+            else:
+                digraphdict['graph']['nodes'][node]= {'metadata': {'label': str(node), 'title': str(node)}}
     
         for edge in newdata[val]:
             if edges.get((edge['source'], edge['target'])):
-                edges[(edge['source'], edge['target'])].append({'weight':edge['weight'], 'lags':val[len(valLag):], 'arrow_color': '#ffffff'})
+                edges[(edge['source'], edge['target'])].append({'weight':edge['weight'], 'lags':val[len(valLag):]})
             else:
-                edges[(edge['source'], edge['target'])]= [{'weight':edge['weight'], 'lags':val[len(valLag):], 'arrow_color': '#ffffff'}]
+                edges[(edge['source'], edge['target'])]= [{'weight':edge['weight'], 'lags':val[len(valLag):]}]
 
     
     for edge in edges:
@@ -144,7 +148,10 @@ def get_math_data(data:dict, linkLag:str, valLag:str, new_names: dict):
             w= sum([e['weight'] for e in edges[edge]])/len(edges[edge])
             digraphdict['graph']['edges'].append({'source': edge[0], 'target': edge[1], 'metadata': {'color': w, 'label':lags, 'hover': str(round(float(w), 4))}})
             node = edge[1]
-            digraphdict['graph']['nodes'][node]= {'metadata': {'label': str(node), 'title': str(node), 'opacity': 0.7, 'border_color': 'black', 'border_size': 2, 'color': 'gray'}}
+            if new_names:
+                digraphdict['graph']['nodes'][node]= {'metadata': {'label': new_names[str(node)], 'title': new_names[str(node)]}}
+            else:    
+                digraphdict['graph']['nodes'][node]= {'metadata': {'label': str(node), 'title': str(node)}}
 
     edge_colors2= colors([c['metadata']['color'] for c in digraphdict['graph']['edges']])
     for edge in digraphdict['graph']['edges']:
@@ -160,19 +167,13 @@ def save_json_data(graphdict:dict, digraphdict:dict, file:str):
     data['digraph']= digraphdict
     save_json(file, data)
 
-
 def verify_node(graphdict:dict,node:str):
     """verifies that each node has the necessary properties to build the graph"""
-    return graphdict["graph"]["nodes"][node]["metadata"].keys() == ["border_color", "border_size", "color", "label", "opacity", "title"]
-
-
-
+    return graphdict["graph"]["nodes"][node]["metadata"].keys() == ["label" "title"]
 
 def verify_edge(graphdict:dict,edge:int):
     """verifies that each edge has the necessary properties to build the graph"""
     return graphdict["graph"]["edges"][edge]["metadata"].keys() == ["color", "hover", "label"] and graphdict["graph"]["edges"][edge].keys()== [ "metadata", "source", "target"]
-
-
 
 def get_json_data(data:dict):
     """gets the graphs from the json file"""
@@ -204,6 +205,70 @@ def get_json_data(data:dict):
 
 
 
+def get_new_names(rename_file: str) -> dict:
+    '''
+    Get a dict with the new names of the nodes
+    :param rename_file: Path to the file with the new names
+    :return: Dict with the new names
+    '''
+    #todo: get .mat of new names
+    #todo: return a dict with {<old name>: <new name>}
+    if rename_file:
+        new_names= load_json_file(rename_file)
+        return {k:v for k, v in zip(new_names.keys(), new_names.values())}
+    else:
+        return None
+
+def get_graphs(file:str, linkLag:str, valLag:str, rename_file:str=None):
+    '''
+    Get the graphs from a .mat or .json file
+    :param file: Path to the file
+    :param linkLag: Name of the link lag
+    :param valLag: Name of the value lag
+    :param rename_file: Path to the file with the new names
+    :return: {'Gdict': <graphdict>, 'Gdidict': <digraphdict>, 'edge_colors': <edge_colors>'}
+    '''
+    _, extension= path.splitext(file.name)
+
+    if extension == '.mat':
+        Gdict, Gdidict, edge_colors = get_math_data(load_mat(file), linkLag, valLag, get_new_names(rename_file))
+
+    if extension == '.json':
+        Gdict, Gdidict, edge_colors = get_json_data(load_json_file(file))
+    
+    sorted_colors = sorted([(round(float(color), 4), edge_colors[color]) for color in edge_colors], key=lambda x: x[0])
+
+    save_json_data(Gdict, Gdidict, join(data_dir, 'graph.json'))
+    return {'Gdict': Gdict, 'Gdidict': Gdidict, 'Edge Colors': sorted_colors}
+
+print([(i,j) for i,j in enumerate(['a', 'b'])])
+
+#fix:
+def brain_3d_graph(G: dict):
+    '''
+    Get a 3d graph of the brain
+    :param G: Graph dict
+    :return: Graph dict
+    '''
+    newG= {
+        'graph': {
+            'nodes': {},
+            'edges': [],
+        }
+    }
+    newG['graph']['metadata']= G['graph']['metadata']
+    newG['graph']['directed']= G['graph']['directed']
+    data= load_json(join(data_dir, 'brain_3d.json'))
+    for node in G['graph']['nodes']:
+        newG['graph']['nodes'][node]= G['graph']['nodes'][node]
+        newG['graph']['nodes'][node]['metadata']['x']= float(data[str(node)]['x'])*3
+        newG['graph']['nodes'][node]['metadata']['y']= float(data[str(node)]['y'])*3
+        newG['graph']['nodes'][node]['metadata']['z']= float(data[str(node)]['z'])*3
+    for edge in G['graph']['edges']:
+        newG['graph']['edges'].append(edge)
+    return newG
+
+
 def get_nodes_graph(G: dict, nodes: list):
     '''
     Get a subgraph of G containing only the nodes in nodes
@@ -233,38 +298,56 @@ def get_nodes_graph(G: dict, nodes: list):
 
 
 
-def get_new_names(rename_file: str) -> dict:
-    '''
-    Get a dict with the new names of the nodes
-    :param rename_file: Path to the file with the new names
-    :return: Dict with the new names
-    '''
-    #todo: get .mat of new names
-    #todo: return a dict with {<old name>: <new name>}
-    return {i:i for i in range(1, 361)}
+def find_max_min_w(wlist: list):
+    max= 0
+    min= 0
+    for w in wlist:
+        if w >= max:
+            max= w
+        if w <= min:
+            min=w
+    return max, min
 
-def get_graphs(file:str, linkLag:str, valLag:str, rename_file:str=None):
+def _int_to_hexa_rgb(n: int, pos : bool = True):  
+    n = str(hex(n))[2:]  if n > 15 else "0" + str(hex(n))[2:]
+    return ('#' + 'ff' + n + n) if pos else ('#' + n + n + 'ff')
+
+def _get_posmin_posmax_neqmin_neqmax(l:list):
     '''
-    Get the graphs from a .mat or .json file
-    :param file: Path to the file
-    :param linkLag: Name of the link lag
-    :param valLag: Name of the value lag
-    :param rename_file: Path to the file with the new names
-    :return: {'Gdict': <graphdict>, 'Gdidict': <digraphdict>, 'edge_colors': <edge_colors>'}
+    Get the positive and a negative min and max of a list
+    :param l: List
+    :return: Position of the min, position of the max, position of the min < 0, position of the max < 0
+    :rtype: tuple
     '''
-    _, extension= path.splitext(file.name)
+    posmin= 2^31
+    posmax= 0
+    neqmin= 0
+    neqmax = -2^32
+    for i in range(len(l)):
+        if l[i] >= 0:
+            if l[i] < posmin:
+                posmin= l[i]
+            if l[i] > posmax:
+                posmax= l[i]
+        else:
+            if l[i] < neqmin:
+                neqmin= l[i]
+            if l[i] > neqmax:
+                neqmax= l[i]        
+    return posmin, posmax, neqmin, neqmax
 
-    if extension == '.mat':
-        Gdict, Gdidict, edge_colors = get_math_data(load_mat(file), linkLag, valLag, get_new_names(rename_file))
-        
+def colors(wlist: list):
+    d = dict()
+    posmin, posmax, neqmin, neqmax=_get_posmin_posmax_neqmin_neqmax(wlist)
+    posrange = posmax if (abs(posmax - posmin) < 10e-10) else posmax - posmin
+    neqrange = neqmax if (abs(neqmax - neqmin) < 10e-10) else neqmax - neqmin
+    for w in wlist:
+        if w >= 0:
+            d[w] = _int_to_hexa_rgb(int((1-((w-posmin)/posrange))*200),True)
+        else:
+            d[w] = _int_to_hexa_rgb(int((((w-neqmin)/neqrange))*200), False)
+    return d
 
-    if extension == '.json':
-        Gdict, Gdidict, edge_colors = get_json_data(load_json_graph(file))
-    
-    sorted_colors = sorted([(round(float(color), 4), edge_colors[color]) for color in edge_colors], key=lambda x: x[0])
-
-    save_json_data(Gdict, Gdidict, join(data_dir, 'graph.json'))
-    return {'Gdict': Gdict, 'Gdidict': Gdidict, 'Edge Colors': sorted_colors}
 
 
 def add_colorbar(color_list: list):
@@ -299,83 +382,3 @@ def add_colorbar(color_list: list):
             graph_img[a][b]= colorbar_img[x][y]
 
     cv2.imwrite(join(data_dir,'graph_colorbar.jpg'), graph_img)
-
-
-#fix:
-def brain_3d_graph(G: dict):
-    '''
-    Get a 3d graph of the brain
-    :param G: Graph dict
-    :return: Graph dict
-    '''
-    newG= {
-        'graph': {
-            'nodes': {},
-            'edges': [],
-        }
-    }
-    newG['graph']['metadata']= G['graph']['metadata']
-    newG['graph']['directed']= G['graph']['directed']
-    data= load_json(join(data_dir, 'brain_3d.json'))
-    for node in G['graph']['nodes']:
-        newG['graph']['nodes'][node]= G['graph']['nodes'][node]
-        newG['graph']['nodes'][node]['metadata']['x']= float(data[str(node)]['x'])*3
-        newG['graph']['nodes'][node]['metadata']['y']= float(data[str(node)]['y'])*3
-        newG['graph']['nodes'][node]['metadata']['z']= float(data[str(node)]['z'])*3
-    for edge in G['graph']['edges']:
-        newG['graph']['edges'].append(edge)
-    return newG
-
-
-def find_max_min_w(wlist: list):
-    max= 0
-    min= 0
-    for w in wlist:
-        if w >= max:
-            max= w
-        if w <= min:
-            min=w
-    return max, min
-
-
-def _int_to_hexa_rgb(n: int, pos : bool = True):  
-    n = str(hex(n))[2:]  if n > 15 else "0" + str(hex(n))[2:]
-    return ('#' + 'ff' + n + n) if pos else ('#' + n + n + 'ff')
-
-
-def _get_posmin_posmax_neqmin_neqmax(l:list):
-    '''
-    Get the positive and a negative min and max of a list
-    :param l: List
-    :return: Position of the min, position of the max, position of the min < 0, position of the max < 0
-    :rtype: tuple
-    '''
-    posmin= 2^31
-    posmax= 0
-    neqmin= 0
-    neqmax = -2^32
-    for i in range(len(l)):
-        if l[i] >= 0:
-            if l[i] < posmin:
-                posmin= l[i]
-            if l[i] > posmax:
-                posmax= l[i]
-        else:
-            if l[i] < neqmin:
-                neqmin= l[i]
-            if l[i] > neqmax:
-                neqmax= l[i]        
-    return posmin, posmax, neqmin, neqmax
-
-
-def colors(wlist: list):
-    d = dict()
-    posmin, posmax, neqmin, neqmax=_get_posmin_posmax_neqmin_neqmax(wlist)
-    posrange = posmax if (abs(posmax - posmin) < 10e-10) else posmax - posmin
-    neqrange = neqmax if (abs(neqmax - neqmin) < 10e-10) else neqmax - neqmin
-    for w in wlist:
-        if w >= 0:
-            d[w] = _int_to_hexa_rgb(int((1-((w-posmin)/posrange))*200),True)
-        else:
-            d[w] = _int_to_hexa_rgb(int((((w-neqmin)/neqrange))*200), False)
-    return d
